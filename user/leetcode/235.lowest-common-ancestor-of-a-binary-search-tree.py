9066bf9dc056d369a7b1801c6298287d60ecc2ea
nuoheng <nuohengluo@qq.com>
2025-02-20 19:29:59 +0800

    Update 235, 236

---
+++ user/leetcode/235.lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=235 lang=python3
+#
+# [235] Lowest Common Ancestor of a Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if q.val < p.val: p, q = q, p
+        
+        if root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif root.val < p.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+        
+# @lc code=end
+
---
+++ user/leetcode/236.lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=236 lang=python3
+#
+# [236] Lowest Common Ancestor of a Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == q or root == p or root == None:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
+            return root
+        
+        if left and right == None:
+            return left
+        elif right and left == None:
+            return right
+        else: 
+            return None
+        
+# @lc code=end
+
